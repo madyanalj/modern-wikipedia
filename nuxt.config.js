c907b87e4fbac5657b402e2f14975d50b5650b78
Maddy Alj <22075444+maddyalj@users.noreply.github.com>
2018-06-04 19:21:03 +0100

    Added nuxt, sass, pug and normalize.css

---
+++ nuxt.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  head: {
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0',
+      },
+    ],
+  },
+  css: [
+    'normalize.css',
+  ],
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  h1 Hello {{ name }}!
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: 'World!',
+  }),
+}
+</script>
+
+<style lang="sass">
+  h1
+    color: blue
+</style>
